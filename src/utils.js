674f74990b02fe6430f2c55fa546909c8162fc43
Unknown <kalt.steven@gmail.com>
2017-12-31 17:25:44 -0500

    upgraded prop checks
    prop checks now can accept a broader range of valid date/time-like objects.

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,55 @@
-export const isDate = {
-  validator(value) {
-    return !Number.isNaN(new Date(value));
+
+export function  testDateValid(date){
+  return (date instanceof Date) &&  !Number.isNaN(date.getDate());
+}
+function  testTimeString(str){
+  return /^(0|1)\d:[0-5]\d$/.test(str)
+}
+function testDateString(str) {
+  return /^\d{4}-\d{2}-\d{2}$/.test(str);
+}
+
+export function emit(value){
+  this.computedValue = value;
+  this.$emit('input', this.computedValue);
+}
+
+export function data(){
+  return {computedValue: this.value};
+}
+
+function toLocaleTimeString(d){
+  return d.toLocaleTimeString('en-US', {hour12: false});
+}
+function coerce(obj, strTest, toString) {
+  // debugger;
+  if (obj){
+    if (obj.constructor === String && strTest(obj)) return obj;
+    const date = new Date(obj);
+    if (testDateValid(date)) {
+      return toString(date);
+    }
   }
-};
+}
+export function toTimeString(obj){
+  return coerce(obj, testTimeString, toLocaleTimeString);
+}
+export function toDateString(obj){
+  return coerce(obj, testDateString, (d)=>d.toISOString().split('T')[0]);
+}
+
+
+export const isDate = {
+  validator: testDateValid
+}
+
+export const worksAsDate = {
+  validator: (value) => testDateString(toDateString(value))
+}
+
+export const worksAsTime = {
+  validator: (value) => testTimeString(toTimeString(value))
+}
 
 export const stringOrNumber = {
   type: [String, Number]
